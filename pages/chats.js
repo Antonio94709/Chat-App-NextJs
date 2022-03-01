c1138cd9be1d300744553f8f8702baab48121c4c
Antonio94709 <exilhekkiekassongo6@gmail.com>
2022-03-01 19:23:05 +0200

    8th commit

--- pages/chats.js
+++ pages/chats.js
@@ -1,5 +1,43 @@
-import React from "react";
+import react, { useState, useEffect, useContext } from "react";
 
-export default function Chats() {
-  return <div className="background">chats</div>;
+import { Context } from "../Context";
+
+import { useRouter } from "next/dist/client/router";
+import dynamic from "next/dynamic";
+
+const ChatEngine = dynamic(() =>
+  import("react-chat-engine").then((module) => module.ChatEngine)
+);
+
+const MessageForSocial = dynamic(() =>
+  import("react-chat-engine").then((module) => module.MessageForSocial)
+);
+
+export default function chats() {
+  const { username, secret } = useContext(Context);
+  const [showChat, setShowChat] = useState(false);
+
+  useEffect(() => {
+    if (typeof document !== null) {
+      setShowChat(true);
+    }
+  });
+
+  if (!showChat) return <div />;
+
+  const router = useRouter();
+
+  return (
+    <div className="background">
+      <div className="shadow">
+        <ChatEngine
+          height="calc(100vh - 200px)"
+          projectID="
+35c61da3-3246-470b-a3a4-825a0a34e8a3"
+          userName={username}
+          userSecret={secret}
+        />
+      </div>
+    </div>
+  );
 }
