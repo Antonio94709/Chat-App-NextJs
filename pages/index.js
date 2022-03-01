09ef9ac8edbc4f19b9cf553b01524bd65da0a7a6
Antonio94709 <exilhekkiekassongo6@gmail.com>
2022-03-01 17:49:45 +0200

    third commit

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,27 @@
-import React from "react";
+import React, { useContext } from "react";
+
+import { Context } from "../Context";
+
+import { useRouter } from "next/dist/client/router";
+
+import axios from "axios";
 
 export default function Auth() {
-  return <div className="background">auth</div>;
+  const { username, setUsername, secret, setSecret } = useContext(Context);
+  return (
+    <div className="background">
+      <div className="auth-container">
+        <form className="auth-form" onSubmit={(e) => e.preventDefault}>
+          <div className="auth-title">Next Js chats</div>
+          <div className="input-container">
+            <input
+              placeholder="Email"
+              className="text-input"
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
